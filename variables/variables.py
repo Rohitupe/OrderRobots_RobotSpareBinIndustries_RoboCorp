bc0e1d021f29ea7f9629a79d33f06f9ccb3db5ee
Rohitupe <rohittupe21@gmail.com>
2021-09-26 21:40:44 +0530

    Variables file

---
+++ variables/variables.py
@@ -0,0 +1,10 @@
+# +
+# Delay Variables in Sec.
+Dsmall = 2
+Dmedium = 5
+Dlarge = 10
+
+# Website URL/File name
+WebURL = r"https://robotsparebinindustries.com/"
+File_Name = "orders.csv"
+Robot_Orders = r"#/robot-order"
