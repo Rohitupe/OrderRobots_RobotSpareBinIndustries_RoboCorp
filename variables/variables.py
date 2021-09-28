7352e2db068980357c4d75a999bcf46a9f702039
Rohitupe <rohittupe21@gmail.com>
2021-09-28 22:07:24 +0530

    web element path variables added

--- variables/variables.py
+++ variables/variables.py
@@ -1,10 +1,26 @@
 # +
 # Delay Variables in Sec.
-Dsmall = 2
-Dmedium = 5
-Dlarge = 10
+d_small = 2
+d_medium = 5
+d_large = 10
 
 # Website URL/File name
-WebURL = r"https://robotsparebinindustries.com/"
-File_Name = "orders.csv"
-Robot_Orders = r"#/robot-order"
+web_URL = r"https://robotsparebinindustries.com/"
+file_name = "orders.csv"
+robot_orders = r"#/robot-order"
+
+# +
+# xPath For Popup Message
+popup_window = r'xpath://div[@class="modal-header"]'
+popup_ok_button = r'xpath://div[@class="alert-buttons"]/button[1]'
+
+# Order Your Robot Form Page
+order_robot_identifer = r'//div[@class="container"]/h2'
+head = 'id:head'
+legs = r'//input[@type="number" and @class="form-control"]'
+address = 'id:address'
+order_first = 'id:order'
+order_another = 'id:order-another'
+# -
+
+
